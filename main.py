04044dd527c9a41db34db6f113e1179bc8c582bc
Liklenb <57323281+Liklenb@users.noreply.github.com>
2023-02-23 13:44:16 +0100

    - merge files to main (to respect rules)

--- main.py
+++ main.py
@@ -1,5 +1,143 @@
-import tkinter
-from background import controller
+import tkinter.font
+
+
+def rgb_to_tkinter(rgb: tuple[int, int, int]):
+    """translates an rgb tuple of int to a tkinter friendly color code
+    """
+    return f'#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}'
+
+
+# create a round rectangle
+def round_rectangle(canvas: tkinter.Canvas, x1: int, y1: int, x2: int, y2: int, radius: int = 25, **kwargs):
+    points = [x1 + radius, y1,
+              x1 + radius, y1,
+              x2 - radius, y1,
+              x2 - radius, y1,
+              x2, y1,
+              x2, y1 + radius,
+              x2, y1 + radius,
+              x2, y2 - radius,
+              x2, y2 - radius,
+              x2, y2,
+              x2 - radius, y2,
+              x2 - radius, y2,
+              x1 + radius, y2,
+              x1 + radius, y2,
+              x1, y2,
+              x1, y2 - radius,
+              x1, y2 - radius,
+              x1, y1 + radius,
+              x1, y1 + radius,
+              x1, y1]
+    return canvas.create_polygon(points, **kwargs, smooth=True)
+
+
+def BetterButton(canvas: tkinter.Canvas,
+                 x: int,
+                 y: int,
+                 text: str,
+                 text_color: tuple[int, int, int] = (0, 0, 0),
+                 color: tuple[int, int, int] = (255, 255, 255),
+                 hover_color: tuple[int, int, int] = (200, 200, 200),
+                 hover_text_color: tuple[int, int, int] = (0, 0, 0),
+                 command=None,
+                 padding: tuple[int, int] = (50, 20),
+                 border_radius: int = 50,
+                 font: tkinter.font.Font = None,
+                 anchor: str = "nw", ):
+    self = {"canvas": canvas, "x": x, "y": y, "text": text, "color": color, "hover_color": hover_color,
+            "hover_text_color": hover_text_color, "command": command, "hover": False, "padding": padding,
+            "border_radius": border_radius, "font": font, "text_color": text_color, "anchor": anchor}
+
+    def hover_on(self: dict):
+        self["hover"] = True
+        self["canvas"].itemconfig(self["text_canvas"], fill=rgb_to_tkinter(self["hover_text_color"]))
+        self["canvas"].itemconfig(self["button"], fill=rgb_to_tkinter(self["hover_color"]))
+
+    def hover_off(self: dict):
+        self["hover"] = False
+        self["canvas"].itemconfig(self["button"], fill=rgb_to_tkinter(self["color"]))
+        self["canvas"].itemconfig(self["text_canvas"], fill=rgb_to_tkinter(self["text_color"]))
+
+    def click(self: dict):
+        if self["hover"]:
+            self["command"]()
+
+    def get_anchor_position(self: dict, x: int, y: int):
+        if self["anchor"] == "center":
+            x -= self["size"][0] / 2
+            y -= self["size"][1] / 2
+        elif self["anchor"] == "ne":
+            x -= self["size"][0]
+        elif self["anchor"] == "se":
+            x -= self["size"][0]
+            y -= self["size"][1]
+        elif self["anchor"] == "sw":
+            y -= self["size"][1]
+        elif self["anchor"] == "e":
+            x -= self["size"][0]
+            y -= self["size"][1] / 2
+        elif self["anchor"] == "w":
+            y -= self["size"][1] / 2
+        elif self["anchor"] == "n":
+            x -= self["size"][0] / 2
+        elif self["anchor"] == "s":
+            x -= self["size"][0] / 2
+            y -= self["size"][1]
+        return x, y
+
+    def build(self: dict):
+        if self["hover"]:
+            color = self["hover_color"]
+            text_color = self["hover_text_color"]
+        else:
+            color = self["color"]
+            text_color = self["text_color"]
+        self["button"] = round_rectangle(self["canvas"], self["x"], self["y"],
+                                         self["x"] + self["text_size"][0] + self["padding"][0],
+                                         self["y"] + self["text_size"][1] + self["padding"][1],
+                                         radius=self["border_radius"], fill=rgb_to_tkinter(color))
+        self["text_canvas"] = self["canvas"].create_text(self["x"] + (self["padding"][0] + self["text_size"][0]) / 2,
+                                                         self["y"] + self["padding"][1] / 2,
+                                                         text=self["text"],
+                                                         anchor="n", fill=rgb_to_tkinter(text_color),
+                                                         font=self["font"])
+
+        # bind the mouse events for the text
+        self["canvas"].tag_bind(self["text_canvas"], "<Enter>", lambda e: self["hover_on"](self))
+        self["canvas"].tag_bind(self["text_canvas"], "<Leave>", lambda e: self["hover_off"](self))
+        self["canvas"].tag_bind(self["text_canvas"], "<Button-1>", lambda e: self["click"](self))
+        # bind the mouse events
+        self["canvas"].tag_bind(self["button"], "<Enter>", lambda e: self["hover_on"](self))
+        self["canvas"].tag_bind(self["button"], "<Leave>", lambda e: self["hover_off"](self))
+        self["canvas"].tag_bind(self["button"], "<Button-1>", lambda e: self["click"](self))
+
+    self["hover_on"] = hover_on
+    self["hover_off"] = hover_off
+    self["click"] = click
+    self["get_anchor_position"] = get_anchor_position
+    self["build"] = build
+
+    # get the text size
+    if not font:
+        self["font"] = tkinter.font.Font(family="TkDefaultFont")
+    self["text_size"] = self["font"].measure(self["text"]), self["font"].metrics("linespace")
+
+    self["size"] = self["text_size"][0] + padding[0], self["text_size"][1] + padding[1]
+    self["x"], self["y"] = self["get_anchor_position"](self, self["x"], self["y"])
+    self["build"](self)
+
+    return self
+
+
+def Menu(root: tkinter.Tk):
+    self = {"frame": tkinter.Frame(root)}
+    self["frame"].grid(row=0, column=0)
+    self["canvas"] = tkinter.Canvas(self["frame"], width=root.winfo_width(), height=root.winfo_height())
+    self["canvas"].pack()
+    self["canvas"].create_text(root.winfo_width() // 2, 20, anchor="n", text="2048", font=tkinter.font.Font(size=100))
+    BetterButton(self["canvas"], root.winfo_width() // 2, root.winfo_height() // 2, "Commencer", anchor="s",
+                 padding=(200, 50))
 
 
 def main():
@@ -7,12 +145,13 @@ def main():
     root = tkinter.Tk()
     # set the title and the size
     root.title('2048')
-    root.state('zoomed')
-    root.resizable(False, False)
     # update the root to get the size later
     root.update()
-    # create the frame controller
-    controller.Controller(root)
+    root.state('zoomed')
+    root.resizable(False, False)
+
+    Menu(root)
+
     root.mainloop()
 
 
