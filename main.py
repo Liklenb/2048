e90e3c4435ee8e639c372128d817b89a9670bebe
uvsq22200432 <arthurborges2003@gmail.com>
2023-03-12 11:17:09 +0100

    -Added a GUI for 2048 -fixed comments.

--- main.py
+++ main.py
@@ -434,7 +434,7 @@ def Game(root: tkinter.Tk):
     self["grid"] = Grid()
     self["grid"]["start"](self["grid"])
 
-    # Color of 2 and 4
+    # Color of the tiles.
     self["color"] = {2: "#eee4da", 4: "#ede0c8", 8: "#f2b179", 16: "#f59563", 32: "#f67c5f", 64: "#f65e3b", 128: "#edcf72",
              256: "#edcc61", 512: "#edc850", 1024: "#edc53f", 2048: "#edc22e"}
 
