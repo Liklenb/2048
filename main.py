5fa6e94bfbd6eeac290250e60ef8723ef43ecc25
Alexandre FRANCOIS <alexandre.francois@ens.uvsq.fr>
2023-04-20 11:10:49 +0200

    - Correction of problem

--- main.py
+++ main.py
@@ -78,7 +78,8 @@ def load():
 
             for y in range(4):
 
-                if info["matrix"][x][y] not in [0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]:
+                if info["matrix"][x][y] not in [0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384,
+                                                32768, 65536, 131072]:
                     return False
 
                 score_verify += info["matrix"][x][y]
